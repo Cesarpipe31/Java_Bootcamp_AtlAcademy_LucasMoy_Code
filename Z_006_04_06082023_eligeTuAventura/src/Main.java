a134b62892b39de6bda28f6e743d44bf90fb7929
Cesarpipe31 <cesarpipe31ingenieria@gmail.com>
2023-06-09 15:37:37 -0500

    publicando

---
+++ Z_006_04_06082023_eligeTuAventura/src/Main.java
@@ -0,0 +1,39 @@
+/*
+4)	Crear un programa que cuente una historia al estilo de “Elige tu propia Aventura”.
+
+Los libros de “Elige tu propia aventura” eran historias interactivas, en el que el lector en un
+momento podía elegir a qué página ir. El ejercicio consiste en hacer un pequeño cuento de este estilo.
+Por ejemplo:
+
+Es una noche estrellada, te encuentras en un bosque misterioso:
+1) Exploras la oscuridad en busca de la fuente de un susurro escalofriante
+2) Sigues el sendero iluminado hacia una pequeña cabaña acogedora
+
+El usuario escoge “2”:
+Decides seguir el sendero iluminado hacia la pequeña cabaña acogedora. Al llegar a la cabaña,
+te das cuenta de que hay dos puertas:
+1) Entras por la puerta de la izquierda
+2) Optas por la puerta de la derecha
+
+El usuario escoge “1”:
+Al abrir la puerta de la izquierda, te encuentras con una habitación llena de tesoros
+brillantes y mágicos. Te maravillas con las joyas y los objetos preciosos que llenan la sala.
+Te das cuenta de que has encontrado el legendario tesoro perdido del bosque. Con tu corazón lleno
+de emoción y riquezas, decides regresar a casa para compartir tu increíble aventura con el mundo.
+¡Felicidades, has tenido un final exitoso en tu búsqueda!
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}
